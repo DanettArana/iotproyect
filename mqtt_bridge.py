e24affb4bd4a9304d92404bde848d7882e81350e
Dainiz <dainizrai@gmail.com>
2025-11-30 23:44:31 -0700

    Proyecto IoT Terminado

--- mqtt_bridge.py
+++ mqtt_bridge.py
@@ -4,6 +4,13 @@ import paho.mqtt.client as mqtt
 
 API_URL = "http://127.0.0.1:8000/monitoreo/recibir/"
 
+# Cuando conecta al broker
+def on_connect(client, userdata, flags, rc):
+    print("Conectado MQTT. Código:", rc)
+    client.subscribe("sonora/sensores/#")   # suscribirse a todos los sensores
+    print("Suscrito a: sonora/sensores/#")
+
+# Cuando llega un mensaje MQTT
 def on_message(client, userdata, msg):
     try:
         raw = msg.payload.decode()
@@ -19,7 +26,6 @@ def on_message(client, userdata, msg):
 
         print(f"Recibido MQTT → {municipio} | {tipo} | {valor}")
 
-        # Construir JSON correcto
         payload = {
             "ciudad": municipio,
             "tipo": tipo,
@@ -27,20 +33,22 @@ def on_message(client, userdata, msg):
         }
 
         r = requests.post(API_URL, json=payload)
-
         print("Respuesta Django:", r.text)
 
     except Exception as e:
         print("Error procesando mensaje:", e)
 
 
+# -------------------
+# CONFIGURACIÓN MQTT
+# -------------------
+
 client = mqtt.Client()
+client.on_connect = on_connect
 client.on_message = on_message
 
+print("Conectando al broker...")
 client.connect("broker.hivemq.com", 1883, 60)
-
-# Suscripción al topic correcto
-client.subscribe("sonora/sensores")
 
 print("Escuchando MQTT...")
 client.loop_forever()
